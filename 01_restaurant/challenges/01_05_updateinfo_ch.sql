51096f0675c7fb9f3a0be3632cb1cc24a0cc980c
Farhoud Khoshnoud <farhoudkhf@gmail.com>
2025-02-08 13:50:50 -0800

    c04-ch1_5

---
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -0,0 +1,13 @@
+-- Update a customer's contact information.
+
+-- Taylor Jenkins, of 27170 6th Ave., Washington, DC,
+-- has moved to 74 Pine St., New York, NY.
+
+UPDATE Customers
+SET Address = '74 Pine St.', City='New York', State='NY'
+WHERE FirstName = 'Taylor' 
+AND LastName = 'Jenkins' 
+AND Address = '27170 6th Ave.';
+
+SELECT * FROM Customers
+WHERE Customers.FirstName = 'Taylor';
