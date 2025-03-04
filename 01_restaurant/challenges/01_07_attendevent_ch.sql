cf7a6628f447b5f2d447a7ea9e9fd5ec18987f99
Farhoud Khoshnoud <farhoudkhf@gmail.com>
2025-03-04 12:41:55 -0800

    c06-ch1-7

---
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -0,0 +1,26 @@
+-- Register a customer for our Anniversary event.
+
+-- The customer 'atapley2j@kinetecoinc.com' will be in
+-- attendance, and will bring 3 friends.
+
+SELECT name FROM sqlite_schema
+WHERE TYPE = 'table';
+
+SELECT * FROM AnniversaryAttendees;
+
+PRAGMA table_info(AnniversaryAttendees);
+
+INSERT INTO AnniversaryAttendees (CustomerID, PartySize)
+VALUES (
+    (SELECT CustomerID
+    FROM Customers
+    WHERE Email = 'atapley2j@kinetecoinc.com'),
+    3
+);
+
+SELECT CustomerID
+FROM Customers
+WHERE Email = 'atapley2j@kinetecoinc.com';
+
+DELETE FROM AnniversaryAttendees AS AA
+WHERE AA.CustomerID = 92 AND AA.PartySize = 3;
