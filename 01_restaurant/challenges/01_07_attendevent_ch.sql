673fa9bbf8a3c3a2713c7b77e731e1ed3593dfaa
Farhoud Khoshnoud <farhoudkhf@gmail.com>
2025-03-15 12:51:11 -0700

    c09-ch1-7

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -3,11 +3,13 @@
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
 
+-- get list of the tables in the schema
 SELECT name FROM sqlite_schema
 WHERE TYPE = 'table';
 
 SELECT * FROM AnniversaryAttendees;
 
+-- get table info
 PRAGMA table_info(AnniversaryAttendees);
 
 INSERT INTO AnniversaryAttendees (CustomerID, PartySize)
