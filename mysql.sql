f166bdfc2588bd3895b5e2e310bd4f2635487232
Farhoud Khoshnoud <farhoudkhf@gmail.com>
2025-03-17 10:57:08 -0700

    c11-mydbExercise2

--- mysql.sql
+++ mysql.sql
@@ -16,10 +16,10 @@ VALUES(date('now','localtime'), 'Friday', 'TEST_02 using the local date');
 PRAGMA table_info(emp2);
 
 ALTER TABLE emp2
-RENAME COLUMN emp_name to FirsName;
+RENAME COLUMN FirsName to FirstName;
 
 ALTER TABLE emp2
-ADD LastName varchar(255);
+ADD email varchar(255) NOT NULL;
 
 ALTER TABLE emp2
 ALTER COLUMN LastName TEXT NOT Null;
@@ -27,7 +27,35 @@ ALTER COLUMN LastName TEXT NOT Null;
 ALTER TABLE emp2
 DROP COLUMN emp_Email;
 
-SELECT * FROM MyCalendar;
+INSERT INTO emp2(empID, FirstName, LastName,  email) 
+VALUES(9111, "Xx", "Yy", "xx.yy@test.co.uk");
+
+SELECT * FROM emp2;
+
+IF(SELECT COUNT(*) FROM 
+
+SELECT exists(SELECT 1 FROM emp2 WHERE email = 'OzxbcwfEnuehjy@gmail.com') AS row_exists;
+
+
+SELECT 
+    CASE
+        WHEN EXISTS (SELECT 1 FROM emp2 WHERE email = 'Ozxbcwf.Enuehjy@gmail.com') 
+        THEN 'Exist'
+        ELSE 'Does not'
+    END;
+
+
+INSERT INTO emp2(empID, FirstName, LastName,  email) 
+VALUES(9112, "Ozxbcwf","Enuehjy","Ozxbcwf.Enuehjy@gmail.com");
+INSERT INTO emp2(empID, FirstName, LastName,  email) 
+VALUES(9113, "Kmadwbc","Jfqqbezpxc","Kmadwbc.Jfqqbezpxc@live.com");
+
+IF NOT EXISTS(Select email from emp2 where email="Ozxbcwf.Enuehjy@gmail.com")
+BEGIN
+INSERT INTO emp2(empID, FirstName, LastName,  email) 
+VALUES(9112, "Ozxbcwf","Enuehjy","Ozxbcwf.Enuehjy@gmail.com")
+END;
+
 
 SELECT name FROM sqlite_schema
 WHERE type='table';
